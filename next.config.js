679a155092318a634ad75457dd70dc01f3cc9303
m1ul24 <toyokazumiura@gmail.com>
2022-07-05 21:19:38 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/m1ul24" : "",
+  trailingSlash: true,
+  experimental: {
+    images: {
+      unoptimized: true
+    }
+  }
 }
 
 module.exports = nextConfig
