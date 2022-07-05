d0fa4514423b6387b067d14eb2551d6b02e8fa12
m1ul24 <toyokazumiura@gmail.com>
2022-07-06 01:35:56 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/m1ul24" : "",
   trailingSlash: true,
   experimental: {
     images: {
