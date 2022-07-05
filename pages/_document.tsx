c5780c1adfc0d58c6394b458bf60cf6b605a1040
m1ul24 <toyokazumiura@gmail.com>
2022-07-06 01:27:34 +0900

    update

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="dark:bg-slate-800 text-slate-100">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
